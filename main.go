d75da99aefcd4f332d8508c33fc119572c808c81
athul <athul8720@gmail.com>
2019-11-23 20:40:14 +0530

    Formatted

--- main.go
+++ main.go
@@ -1,63 +1,64 @@
 package main
 
 import (
-	"io/ioutil"
-	"html/template"
 	"fmt"
-	"net/http"
+	"html/template"
+	"io/ioutil"
 	"log"
-
+	"net/http"
 
 	//"github.com/gin-contrib/sse"
-    "github.com/gin-gonic/gin"
-    "gopkg.in/russross/blackfriday.v2"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/russross/blackfriday.v2"
 )
-type Post struct{
+
+type Post struct {
 	//blog post
-	Title string
+	Title   string
 	Content template.HTML
 }
-func main(){
-	r:=gin.Default()
+
+func main() {
+	r := gin.Default()
 	r.Use(gin.Logger())
-	r.Delims("{{","}}")
+	r.Delims("{{", "}}")
 	r.LoadHTMLGlob("./templates/*.tmpl.html")
 
-	r.GET("/",func (c *gin.Context){
+	r.GET("/", func(c *gin.Context) {
 		var posts []string
-		files,err:=ioutil.ReadDir("./markdown/")
-		if err!=nil{
+		files, err := ioutil.ReadDir("./markdown/")
+		if err != nil {
 			log.Fatal(err)
 		}
-		for _,file:=range files{
+		for _, file := range files {
 			fmt.Println(file.Name())
-			posts=append(posts,file.Name())
+			posts = append(posts, file.Name())
 		}
-		c.HTML(http.StatusOK,"index.tmpl.html",gin.H{
-			"posts":posts,
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"posts": posts,
 		})
 	})
 	r.GET("/:postName", func(c *gin.Context) {
 		postName := c.Param("postName")
-	  
+
 		mdfile, err := ioutil.ReadFile("./markdown/" + postName)
-	  
+
 		if err != nil {
-		  fmt.Println(err)
-		  c.HTML(http.StatusNotFound, "error.tmpl.html", nil)
-		  c.Abort()
-		  return
+			fmt.Println(err)
+			c.HTML(http.StatusNotFound, "error.tmpl.html", nil)
+			c.Abort()
+			return
 		}
-	  
+
 		postHTML := template.HTML(blackfriday.Run([]byte(mdfile)))
-	  
+
 		post := Post{Title: postName, Content: postHTML}
-	  
+
 		c.HTML(http.StatusOK, "post.tmpl.html", gin.H{
-		  "Title":   post.Title,
-		  "Content": post.Content,
+			"Title":   post.Title,
+			"Content": post.Content,
 		})
-	  })
+	})
 	r.Run()
 
 }
---
+++ markdown/Kilipoyi.md
@@ -0,0 +1,3 @@
+# Aaaaaaaaaaaaaa
+
+## Kili Poyi
